dd27ee9f1bb1fd33c478a824b819c57db0dec2c9
Solvita Dvorcova <solvita.dvorcova@accenture.com>
2025-09-11 16:21:44 +0300

    streamig

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,14 @@
-import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import CardWrapper from "@/app/ui/dashboard/cards";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lustiana } from "@/app/ui/fonts";
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
 import { CardData } from "@/app/lib/definitions";
+import { Suspense } from "react";
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+  CardsSkeleton,
+} from "@/app/ui/skeletons";
 
 const defaultCardData: CardData = {
   totalPaidInvoices: "$0",
@@ -13,27 +18,6 @@ const defaultCardData: CardData = {
 };
 
 export default async function Page() {
-const [revenue, latestInvoices, cardData] = await Promise.all([
-  fetchRevenue().catch((err): any[] => {
-    console.error("Error fetching revenue:", err);
-    return [];
-  }),
-  fetchLatestInvoices().catch((err): any[] => {
-    console.error("Error fetching latest invoices:", err);
-    return [];
-  }),
-  fetchCardData().catch((err): CardData => {
-    console.error("Error fetching card data:", err);
-    return defaultCardData;
-  }),
-]);
-  
-  const {
-    totalPaidInvoices,
-    totalPendingInvoices,
-    numberOfInvoices,
-    numberOfCustomers,
-  } = cardData;
   
   return (
     <main>
@@ -41,18 +25,17 @@ const [revenue, latestInvoices, cardData] = await Promise.all([
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
